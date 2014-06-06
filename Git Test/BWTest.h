2fc53ae6686a13b4e90e3249959a9b9a6dce4f9b
Benjamin West <benjaminwest@2013dellwin8cp.solutiondesign.local>
2014-06-06 09:45:03 -0500

    added a test class

---
+++ Git Test/BWTest.h
@@ -0,0 +1,13 @@
+//
+//  BWTest.h
+//  Git Test
+//
+//  Created by Benjamin West on 6/6/14.
+//  Copyright (c) 2014 West. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BWTest : NSObject
+
+@end
---
+++ Git Test/BWTest.m
@@ -0,0 +1,13 @@
+//
+//  BWTest.m
+//  Git Test
+//
+//  Created by Benjamin West on 6/6/14.
+//  Copyright (c) 2014 West. All rights reserved.
+//
+
+#import "BWTest.h"
+
+@implementation BWTest
+
+@end
